e437e38b8eb80aad1ea191ab5a50d6641b86e91b
Gustavo Frohlich <gustavo.frohlich@hotmail.com>
2020-12-17 17:45:45 -0300

    Implementando autorização

--- src/main/java/com/gustavo/cursomc/config/SecurityConfig.java
+++ src/main/java/com/gustavo/cursomc/config/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.gustavo.cursomc.security.JWTAuthenticationFilter;
+import com.gustavo.cursomc.security.JWTAuthorizationFilter;
 import com.gustavo.cursomc.security.JWTUtil;
 
 
@@ -59,6 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
